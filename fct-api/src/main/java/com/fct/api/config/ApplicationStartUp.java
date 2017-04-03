f176fb3383d144d05eaa7a3c6a089b4e456a0b8f
qiujun <qiujun@wondersgroup.com>
2017-04-03 13:59:50 +0800

    project init

---
+++ fct-api/src/main/java/com/fct/api/config/ApplicationStartUp.java
@@ -0,0 +1,30 @@
+package com.fct.api.config;
+
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by nick on 2017/4/3.
+ * @author nick
+ */
+@SpringBootApplication(scanBasePackages = "com.fct")
+@ImportResource({"classpath:dubbo/dubbo-consumer.xml"})
+public class ApplicationStartUp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationStartUp.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(ApplicationStartUp.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+}
